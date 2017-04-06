b88c6c7e03b2a5b41c73208395d16999cd45e6bf
bigmeco <bigmecoprog@gmail.com>
2017-04-06 16:40:34 +0300

    Prob

--- RandomFilms/app/src/main/java/bigi/randomfilms/MainActivity.java
+++ RandomFilms/app/src/main/java/bigi/randomfilms/MainActivity.java
@@ -2,12 +2,24 @@ package bigi.randomfilms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+    KenBurnsView kbv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        kbv = (KenBurnsView) findViewById(R.id.backdrop_path);
+
+        Picasso.with(this)
+                .load("https://image.tmdb.org/t/p/w500/5pAGnkFYSsFJ99ZxDIYnhQbQFXs.jpg")
+                .placeholder(R.drawable.fon)
+                .error(R.drawable.fon)
+                .into(kbv);
     }
 }
