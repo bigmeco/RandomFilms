bd5ce80d0531831b67f50dd1fd7a5eb4697524b4
bigmeco <bigmecoprog@gmail.com>
2017-04-06 23:05:58 +0300

    POJO2

---
+++ RandomFilms/app/src/main/java/bigi/randomfilms/POJO2/Example.java
@@ -0,0 +1,22 @@
+
+package bigi.randomfilms.POJO2;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Example {
+
+    @SerializedName("genres")
+    @Expose
+    private List<Genre> genres = null;
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+}
