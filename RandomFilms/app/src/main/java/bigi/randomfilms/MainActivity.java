6db4be713a0de20ca14ba9246bd3fb8122aae7b9
bigmeco <bigmecoprog@gmail.com>
2017-04-06 21:16:41 +0300

    ok

--- RandomFilms/app/src/main/java/bigi/randomfilms/MainActivity.java
+++ RandomFilms/app/src/main/java/bigi/randomfilms/MainActivity.java
@@ -2,6 +2,9 @@ package bigi.randomfilms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     TextView original_title;
     TextView overview;
     TextView vote_count;
+    EditText editText;
+    ImageButton imageButton;
+    int ID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,57 +42,65 @@ public class MainActivity extends AppCompatActivity {
         original_title =(TextView) findViewById(R.id.original_title);
         overview =(TextView) findViewById(R.id.overview);
         vote_count =(TextView) findViewById(R.id.vote_count);
+        editText =(EditText) findViewById(R.id.editText);
 
+
+        imageButton = (ImageButton)findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            ID = Integer.parseInt(editText.getText().toString());
+            ResstrPr();
+
+        }
+        });
+
+
+
+
+
+
+    }
+
+    public void ResstrPr() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         InterfFilm service = retrofit.create(InterfFilm.class);
-        Call<Example> call = service.listRepos(299);
+        Call<Example> call = service.listRepos(ID);
         call.enqueue(new Callback<Example>() {
-                         @Override
-                         public void onResponse(Call<Example> call, Response<Example> response) {
-                             System.out.println(response.body().getOverview());
+            @Override
+            public void onResponse(Call<Example> call, Response<Example> response) {
+                System.out.println(response.body().getOverview());
 
-                             Picasso.with(MainActivity.this)
-                                     .load(Imeg+response.body().getBackdropPath())
-                                     .placeholder(R.drawable.fon)
-                                     .error(R.drawable.fon)
-                                     .into(kbv);
+                Picasso.with(MainActivity.this)
+                        .load(Imeg+response.body().getBackdropPath())
+                        .placeholder(R.drawable.fon)
+                        .error(R.drawable.fon)
+                        .into(kbv);
 
-                             Picasso.with(MainActivity.this)
-                                     .load(Imeg+response.body().getPosterPath())
-                                     .placeholder(R.drawable.fon)
-                                     .error(R.drawable.fon)
-                                     .into(imageView);
-                             title.setText(response.body().getTitle());
-                             original_title.setText(response.body().getOriginalTitle());
-                             overview.setText(response.body().getOverview());
-                           //  vote_count.setText(response.body().getVoteCount());
+                Picasso.with(MainActivity.this)
+                        .load(Imeg+response.body().getPosterPath())
+                        .placeholder(R.drawable.fon)
+                        .error(R.drawable.fon)
+                        .into(imageView);
+                title.setText(response.body().getTitle());
+                original_title.setText(response.body().getOriginalTitle());
+                overview.setText(response.body().getOverview());
 
-
-                         }
-
-                         @Override
-                         public void onFailure(Call<Example> call, Throwable t) {
-
-                         }
-                     });
-//        Response<Example> response = null;
-//        try {
-//            response = call.execute();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        Example map = response.body();
-//        System.out.println(map.getOverview());
+                //  vote_count.setText(response.body().getVoteCount());
 
 
-        Picasso.with(this)
-                .load("https://image.tmdb.org/t/p/w500/5pAGnkFYSsFJ99ZxDIYnhQbQFXs.jpg")
-                .placeholder(R.drawable.fon)
-                .error(R.drawable.fon)
-                .into(kbv);
+            }
+
+            @Override
+            public void onFailure(Call<Example> call, Throwable t) {
+
+            }
+        });
+
     }
 }
+
+
