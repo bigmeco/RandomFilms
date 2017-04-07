81fcc9cab0de63756a8ecb7a42b0602479eb8986
bigmeco <bigmecoprog@gmail.com>
2017-04-07 18:32:34 +0300

    KnopRAND

--- RandomFilms/app/src/main/java/bigi/randomfilms/InterfFilm.java
+++ RandomFilms/app/src/main/java/bigi/randomfilms/InterfFilm.java
@@ -1,10 +1,12 @@
 package bigi.randomfilms;
 
 import bigi.randomfilms.POJO.Example;
+import bigi.randomfilms.POJOrandom.ExampleRandom;
 import retrofit2.Call;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by bigi on 06.04.2017.
@@ -14,4 +16,7 @@ public interface InterfFilm {
 
     @GET("movie/{movie_id}?api_key=ba8e8a114ce7fc27aa71ebec8c0b1afe&language=ru-RU")
     Call<Example> listRepos(@Path("movie_id") int groupId);
+
+    @GET("discover/movie?api_key=ba8e8a114ce7fc27aa71ebec8c0b1afe&language=ru-RU&sort_by=popularity.desc&include_adult=false&include_video=false&")
+    Call<ExampleRandom> RandomZp(@Query("page") int Rj);
 }
--- RandomFilms/app/src/main/java/bigi/randomfilms/MainActivity.java
+++ RandomFilms/app/src/main/java/bigi/randomfilms/MainActivity.java
@@ -12,8 +12,11 @@ import com.flaviofaria.kenburnsview.KenBurnsView;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.util.Random;
 
 import bigi.randomfilms.POJO.Example;
+import bigi.randomfilms.POJOrandom.ExampleRandom;
+import bigi.randomfilms.POJOrandom.ResultRandom;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -23,14 +26,17 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private static final String URL = "https://api.themoviedb.org/3/";
     private static final String Imeg = "https://image.tmdb.org/t/p/w500";
+    private static  int srtoni,nomer;
     KenBurnsView kbv;
     ImageView imageView;
     TextView title;
     TextView original_title;
     TextView overview;
-    TextView vote_count;
+    TextView release_date;
     EditText editText;
     ImageButton imageButton;
+    ImageButton imageButton2;
+    final Random random = new Random();
     int ID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         title =(TextView) findViewById(R.id.title);
         original_title =(TextView) findViewById(R.id.original_title);
         overview =(TextView) findViewById(R.id.overview);
-        vote_count =(TextView) findViewById(R.id.vote_count);
+        release_date =(TextView) findViewById(R.id.release_date);
         editText =(EditText) findViewById(R.id.editText);
 
 
@@ -51,23 +57,42 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             ID = Integer.parseInt(editText.getText().toString());
             ResstrPr();
+            System.out.println(random.nextInt(999));
+            System.out.println(random.nextInt(19));
 
         }
+        });
+
+        imageButton2 = (ImageButton)findViewById(R.id.imageButton2);
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                srtoni = random.nextInt(999);
+                srtoni++;
+                nomer = random.nextInt(19);
+                nomer++;
+
+                RandomSbor(srtoni,nomer);
+            }
         });
 
 
 
 
-
-
     }
-
-    public void ResstrPr() {
+    public InterfFilm RetrZap() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         InterfFilm service = retrofit.create(InterfFilm.class);
+        return service;
+    }
+
+
+    private void ResstrPr() {
+        InterfFilm service = RetrZap();
+
         Call<Example> call = service.listRepos(ID);
         call.enqueue(new Callback<Example>() {
             @Override
@@ -89,9 +114,7 @@ public class MainActivity extends AppCompatActivity {
                 original_title.setText(response.body().getOriginalTitle());
                 overview.setText(response.body().getOverview());
 
-                //  vote_count.setText(response.body().getVoteCount());
-
-
+                release_date.setText(response.body().getReleaseDate());
             }
 
             @Override
@@ -101,6 +124,43 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    public void RandomSbor(int srtoni, final int nomer) {
+
+        InterfFilm service = RetrZap();
+        Call<ExampleRandom> call = service.RandomZp(srtoni);
+        call.enqueue(new Callback<ExampleRandom>() {
+            @Override
+            public void onResponse(Call<ExampleRandom> call, Response<ExampleRandom> response) {
+                ResultRandom  F = response.body().getResults().get(nomer);
+                Picasso.with(MainActivity.this)
+                        .load(Imeg+F.getBackdropPath())
+                        .placeholder(R.drawable.fon)
+                        .error(R.drawable.fon)
+                        .into(kbv);
+
+                Picasso.with(MainActivity.this)
+                        .load(Imeg+F.getPosterPath())
+                        .placeholder(R.drawable.fon)
+                        .error(R.drawable.fon)
+                        .into(imageView);
+                title.setText(F.getTitle());
+                original_title.setText(F.getOriginalTitle());
+                overview.setText(F.getOverview());
+
+                release_date.setText(F.getReleaseDate());
+
+
+        }
+
+            @Override
+            public void onFailure(Call<ExampleRandom> call, Throwable t) {
+
+            }
+        });
+
+
+    }
 }
+
 
 
