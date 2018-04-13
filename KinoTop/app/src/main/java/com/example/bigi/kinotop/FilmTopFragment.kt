894b8e7f7bba6367b26102b4fbc6cc990b8d2106
bigmeco <bigmecoprog@gmail.com>
2018-04-13 23:28:27 +0300

    start retrofit

--- KinoTop/app/src/main/java/com/example/bigi/kinotop/FilmTopFragment.kt
+++ KinoTop/app/src/main/java/com/example/bigi/kinotop/FilmTopFragment.kt
@@ -4,9 +4,12 @@ package com.example.bigi.kinotop
 import android.app.Fragment
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.film_top_fragment.*
 
 class FilmTopFragment : Fragment() {
@@ -14,8 +17,22 @@ class FilmTopFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         var list = ArrayList<TopFilmData>()
         list.add(TopFilmData("dd"))
+        list.add(TopFilmData("dd"))
+        list.add(TopFilmData("dd"))
+        list.add(TopFilmData("dd"))
+        list.add(TopFilmData("dd"))
 
+        val repository = Service.getFilm()
 
+        repository.listNewFilm()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe ({
+                    result ->
+                    Log.d("Result", "There are ${result.results} ")
+                }, { error ->
+                    error.printStackTrace()
+                })
         topFilmList.layoutManager = LinearLayoutManager(activity)
         topFilmList.adapter = TopFilmAdapter(list,{(itam)->})
     }
@@ -26,7 +43,6 @@ class FilmTopFragment : Fragment() {
         return LayoutInflater.from(container?.context).inflate(R.layout.film_top_fragment, container, false)
 
     }
-
 
 
 }
