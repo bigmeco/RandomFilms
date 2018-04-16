a5c7ed9f47340a025642c6a17d7e5ef01921e014
bigmeco <bigmecoprog@gmail.com>
2018-04-16 16:24:54 +0300

    fix activity

--- KinoTop/app/src/main/java/com/example/bigi/kinotop/RandomFragment.kt
+++ KinoTop/app/src/main/java/com/example/bigi/kinotop/RandomFragment.kt
@@ -2,15 +2,35 @@ package com.example.bigi.kinotop
 
 import android.app.Fragment
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.fragment_random.*
 
 
 class RandomFragment : Fragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        spinner.adapter = ArrayAdapter<String>(activity,R.layout.list_genre,continText())
+         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+             override fun onNothingSelected(parent: AdapterView<*>?) {
+             }
+
+             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                 floatingRepit.setOnClickListener { Log.d("ggg",position.toString()) }
+                 if (position==0) {
+                     floatingRepit.setOnClickListener { Log.d("fff",position.toString())
+
+                     }
+                 }
+             }
+         }
+
 
 
     }
@@ -19,4 +39,14 @@ class RandomFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         return LayoutInflater.from(container?.context).inflate(R.layout.fragment_random, container, false)
     }
+
+    fun continText(): ArrayList<String>  {
+        var genres:ArrayList<String> = ArrayList()
+        genres.add("Все жанры")
+                for (id in GenreList.values()) {
+                    genres.add(id.names)
+        }
+        return genres
+    }
+
 }
