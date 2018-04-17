f0a37cc3c194c6dffa7ba2564478ec06c0786c1d
bigmeco <bigmecoprog@gmail.com>
2018-04-17 14:02:41 +0300

    start random respons retrofit

--- KinoTop/app/src/main/java/com/example/bigi/kinotop/GetFilm.kt
+++ KinoTop/app/src/main/java/com/example/bigi/kinotop/GetFilm.kt
@@ -1,15 +1,17 @@
 package com.example.bigi.kinotop;
 
 
-
 import com.example.bigi.kinotop.data.NewFilm
 import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 
- interface GetFilm {
+interface GetFilm {
     @GET("movie/upcoming?page=1&language=ru-RU&api_key=ba8e8a114ce7fc27aa71ebec8c0b1afe")
     fun listNewFilm(): Observable<NewFilm>
+
+    @GET("discover/movie?api_key=ba8e8a114ce7fc27aa71ebec8c0b1afe&language=ru-RU&sort_by=popularity.desc&page=1000")
+    fun RandomFilm(): Observable<NewFilm>
 
 
 }
--- KinoTop/app/src/main/java/com/example/bigi/kinotop/RandomFragment.kt
+++ KinoTop/app/src/main/java/com/example/bigi/kinotop/RandomFragment.kt
@@ -2,6 +2,7 @@ package com.example.bigi.kinotop
 
 import android.app.Fragment
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,11 @@ import android.view.animation.AnimationUtils
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.fragment_random.*
+import android.view.animation.Animation.AnimationListener
+import com.example.bigi.kinotop.data.NewFilmData
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.film_top_fragment.*
 
 
 class RandomFragment : Fragment() {
@@ -18,43 +24,65 @@ class RandomFragment : Fragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        spinner.adapter = ArrayAdapter<String>(activity,R.layout.list_genre,continText())
-         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-             override fun onNothingSelected(parent: AdapterView<*>?) {
-             }
+        spinner.adapter = ArrayAdapter<String>(activity, R.layout.list_genre, continText())
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
 
-             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                 floatingRepit.setOnClickListener { Log.d("ggg",position.toString()) }
-                 if (position==0) {
-                     floatingRepit.setOnClickListener { Log.d("fff",position.toString())
-                         val animOld = AnimationUtils.loadAnimation(activity,R.anim.repit_film)
-                         fabSpace.setInAnimation(animOld)
-                         animOld.
-                         val animNew = AnimationUtils.loadAnimation(activity,R.anim.rept_new_film)
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                floatingRepit.setOnClickListener { Log.d("ggg", position.toString()) }
+                if (position == 0) {
+                    floatingRepit.setOnClickListener {
+                        Log.d("fff", position.toString())
 
-                         fabSpace.startAnimation(animNew)
+                        changeFilm()
+                        Service.getFilm().listNewFilm()
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribeOn(Schedulers.io())
+                                .subscribe({ result ->
+                                    textNameView.text = result.results!!.get(0)!!.title
+                                }, { error ->
+                                    error.printStackTrace()
+                                })
+                    }
+                }
+            }
+        }
+        floatingLike.setOnClickListener {
+            floatingLike.setImageResource(R.drawable.heart)
 
-                     }
-                 }
-             }
-         }
-
+        }
 
 
     }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return LayoutInflater.from(container?.context).inflate(R.layout.fragment_random, container, false)
     }
 
-    fun continText(): ArrayList<String>  {
-        var genres:ArrayList<String> = ArrayList()
+    fun continText(): ArrayList<String> {
+        var genres: ArrayList<String> = ArrayList()
         genres.add("Все жанры")
-                for (id in GenreList.values()) {
-                    genres.add(id.names)
+        for (id in GenreList.values()) {
+            genres.add(id.names)
         }
         return genres
     }
 
+
+    private fun changeFilm() {
+        val animOld = AnimationUtils.loadAnimation(activity, R.anim.repit_film)
+        val animNew = AnimationUtils.loadAnimation(activity, R.anim.rept_new_film)
+        fabSpace.startAnimation(animOld)
+        animOld.setAnimationListener(object : AnimationListener {
+            override fun onAnimationStart(anim: Animation) {}
+            override fun onAnimationRepeat(anim: Animation) {}
+            override fun onAnimationEnd(anim: Animation) {
+                fabSpace.startAnimation(animNew)
+            }
+        })
+
+    }
 }
