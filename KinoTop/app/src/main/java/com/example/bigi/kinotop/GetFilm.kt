76f8d8bede66d1a973bc0197e74fb61bb5cb2004
bigmeco <bigmecoprog@gmail.com>
2018-04-17 18:14:39 +0300

    start random respons retrofit

--- KinoTop/app/src/main/java/com/example/bigi/kinotop/GetFilm.kt
+++ KinoTop/app/src/main/java/com/example/bigi/kinotop/GetFilm.kt
@@ -5,13 +5,18 @@ import com.example.bigi.kinotop.data.NewFilm
 import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface GetFilm {
     @GET("movie/upcoming?page=1&language=ru-RU&api_key=ba8e8a114ce7fc27aa71ebec8c0b1afe")
     fun listNewFilm(): Observable<NewFilm>
 
-    @GET("discover/movie?api_key=ba8e8a114ce7fc27aa71ebec8c0b1afe&language=ru-RU&sort_by=popularity.desc&page=1000")
-    fun RandomFilm(): Observable<NewFilm>
+    @GET("discover/movie?api_key=ba8e8a114ce7fc27aa71ebec8c0b1afe&language=ru-RU&sort_by=popularity.desc")
+    fun randomFilm(@Query("page") pageList: Int): Observable<NewFilm>
+
+
+    @GET("discover/movie?api_key=ba8e8a114ce7fc27aa71ebec8c0b1afe&language=ru-RU&sort_by=popularity.desc")
+    fun randomGenresFilm(@Query("page") pageList: Int, @Query("with_genres") genreCount: Int): Observable<NewFilm>
 
 
 }
--- KinoTop/app/src/main/java/com/example/bigi/kinotop/RandomFragment.kt
+++ KinoTop/app/src/main/java/com/example/bigi/kinotop/RandomFragment.kt
@@ -14,36 +14,61 @@ import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.fragment_random.*
 import android.view.animation.Animation.AnimationListener
 import com.example.bigi.kinotop.data.NewFilmData
+import com.squareup.picasso.Picasso
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.film_top_fragment.*
+import kotlinx.android.synthetic.main.list_top_layout.view.*
+import java.util.*
 
 
 class RandomFragment : Fragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        requesRrandomFilm()
         spinner.adapter = ArrayAdapter<String>(activity, R.layout.list_genre, continText())
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
             }
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                floatingRepit.setOnClickListener { Log.d("ggg", position.toString()) }
+                floatingRepit.setOnClickListener { Log.d("ggg", position.toString())
+                    val random = Random()
+                    Service.getFilm().randomGenresFilm(random.nextInt(999)+1,16)
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribeOn(Schedulers.io())
+                            .subscribe({ result ->
+//                                val randomRezult = result.results!!.get(random.nextInt(19)+1)!!
+//                                Picasso.with(activity)
+//                                        .load("https://image.tmdb.org/t/p/w500${randomRezult.posterPath}")
+//                                        .placeholder(R.drawable.in_progress)
+//                                        .into(posterView)
+//                                Picasso.with(activity)
+//                                        .load("https://image.tmdb.org/t/p/original${randomRezult.backdropPath}")
+//                                        .placeholder(R.drawable.defaultes)
+//                                        .into(backgroundImageView)
+//                                textNameView.text = randomRezult.title
+//                                textDataView.text = randomRezult.releaseDate
+//                                ratingView.setValueAnimated(randomRezult.voteAverage!!,1500)
+//                                popularityView.setValueAnimated(randomRezult.popularity!!, 1500)
+//                                textGenresView.text = "Жанры: ${continText(randomRezult.genreIds)}"
+//                                textInfoView.text = randomRezult.overview
+
+
+
+                            }, { error ->
+                                error.printStackTrace()
+                            })
+
+
+
+                }
                 if (position == 0) {
                     floatingRepit.setOnClickListener {
                         Log.d("fff", position.toString())
-
                         changeFilm()
-                        Service.getFilm().listNewFilm()
-                                .observeOn(AndroidSchedulers.mainThread())
-                                .subscribeOn(Schedulers.io())
-                                .subscribe({ result ->
-                                    textNameView.text = result.results!!.get(0)!!.title
-                                }, { error ->
-                                    error.printStackTrace()
-                                })
+                        requesRrandomFilm()
                     }
                 }
             }
@@ -83,6 +108,38 @@ class RandomFragment : Fragment() {
                 fabSpace.startAnimation(animNew)
             }
         })
+    }
+
+    private fun requesRrandomFilm() {
+        val random = Random()
+        Service.getFilm().randomFilm(random.nextInt(999)+1)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({ result ->
+                    val randomRezult = result.results!!.get(random.nextInt(19)+1)!!
+                    Picasso.with(activity)
+                            .load("https://image.tmdb.org/t/p/w500${randomRezult.posterPath}")
+                            .placeholder(R.drawable.in_progress)
+                            .into(posterView)
+                    Picasso.with(activity)
+                            .load("https://image.tmdb.org/t/p/original${randomRezult.backdropPath}")
+                            .placeholder(R.drawable.defaultes)
+                            .into(backgroundImageView)
+                    textNameView.text = randomRezult.title
+                    textDataView.text = randomRezult.releaseDate
+                    ratingView.setValueAnimated(randomRezult.voteAverage!!,1500)
+                    popularityView.setValueAnimated(randomRezult.popularity!!, 1500)
+                    textGenresView.text = "Жанры: ${continText(randomRezult.genreIds)}"
+                    textInfoView.text = randomRezult.overview
+
+
+
+                }, { error ->
+                    error.printStackTrace()
+                })
 
     }
+
+
+
 }
